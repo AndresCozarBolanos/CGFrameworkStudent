efbde4d357ff2d5cf95a0f63d87edd0dd80d50e8
AndresCozarBolanos <andres.cozar01@estudiant.upf.edu>
2026-03-08 15:55:13 +0100

    last two

--- res/shaders/quads/quad3_2.fs
+++ res/shaders/quads/quad3_2.fs
@@ -3,12 +3,12 @@ uniform sampler2D u_texture;
 uniform float u_time;
 
 void main() {
-
+    // We want to create a rotating version of the texture
     vec2 uv = v_uv - 0.5;
 
-    float angulo = u_time * 0.2;
-    float s = sin(angulo);
-    float c = cos(angulo);
+    float alpha = u_time * 0.2;
+    float s = sin(alpha);
+    float c = cos(alpha);
 
     vec2 uv_rot;
     uv_rot.x = uv.x * c - uv.y * s;
@@ -17,10 +17,12 @@ void main() {
 
     vec4 color = texture2D(u_texture, uv_rot);
 
-    float estado = mod(floor(u_time), 3.0);
+    // One we have the rotated color, we want to create a traffic light effect 
+    // by multiplying the color with a vector that changes over time
+    float state = mod(floor(u_time), 3.0);
 
-    float r = step(0.5, estado);
-    float g = 1.0 - step(0.5, estado) + step(1.5, estado);
+    float r = step(0.5, state);
+    float g = 1.0 - step(0.5, state) + step(1.5, state);
 
     vec3 semaforo = vec3(r, g, 0.0);
 
